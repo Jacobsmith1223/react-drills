b196f2ac72f3e3b6153abaa66d7f04f912ce04c3
Jake Smith <jacob_smith1223@me.com>
2019-08-06 14:14:03 -0600

    app-5 done

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react"
 import "./App.css";
+import Image from'./img-component/Image'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Image image={'https://pics.me.me/nothing-to-see-here-just-two-legends-smiling-at-each-58267562.png'}/>
       </div>
     );
   }
