42b7234e985d7b00ad938bc7df5da17a52ac740e
Jake Smith <jacob_smith1223@me.com>
2019-08-06 13:21:42 -0600

    did app 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,17 +3,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state={
+      words: ' '
+    }
+  }
+
+  words= (event) => {
+    this.setState({words: event.target.value})
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <div className='App'>
+       <input type='text' onChange={this.words} />
+       <h1>{this.state.words}</h1>
+     </div>
     );
   }
 }
